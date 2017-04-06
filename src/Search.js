adebdfcd38168017c55899cf4f3818e8054c8575
Saidur Rahman <saidur2k@hotmail.com>
2017-04-06 20:40:32 +1000

    add bootstrap

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-
+import { FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap'
 export default class Search extends Component {
   render () {
     return (
       <form onSubmit={this.props.handleSubmit}>
-        <input
-          type="text"
-          value={this.props.searchQuery}
-          onChange={this.props.handleChange}
-        />
-        <button>Search</button>
+        <FormGroup controlId="formBasicText">
+          <ControlLabel>Enter name</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.props.searchquery}
+            placeholder="John Doe"
+            onChange={this.props.handleChange}
+          />
+          <FormControl.Feedback />
+          <Button bsStyle="primary">Search</Button>
+        </FormGroup>
       </form>
     )
   }
